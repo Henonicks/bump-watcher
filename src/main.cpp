5545629d10c34252278263f9afdbaba6de126c43
Henonicks <sawsawsa777@gmail.com>
2023-10-31 18:38:56 +0200

    Removed the unnecessary testing stuff

--- src/main.cpp
+++ src/main.cpp
@@ -13,16 +13,6 @@ int main() {
 
     bot.on_log(dpp::utility::cout_logger());
 
-    bot.on_slashcommand([](const dpp::slashcommand_t& event) -> void {
-        if (event.command.get_command_name() == "bump") {
-            dpp::embed embed = dpp::embed()
-                    .set_color(dpp::colors::sti_blue)
-                    .set_title("Title")
-                    .set_image("https://disboard.org/images/bot-command-image-bump.png");
-            event.reply(dpp::message().add_embed(embed));
-        }
-    });
-
     bot.on_message_create([&bot, role_id](const dpp::message_create_t& event) -> void {
         const dpp::message& msg = event.msg;
         if (msg.author.id == DISBOARD_ID && msg.embeds.size() == 1) {
@@ -45,13 +35,6 @@ int main() {
         }
     });
 
-    bot.on_ready([&bot](const dpp::ready_t&) -> void {
-        if (dpp::run_once <struct register_bot_commands>()) {
-            bot.global_bulk_command_create({ dpp::slashcommand("bump", "bump", bot.me.id) });
-        }
-    });
-
     bot.start(dpp::st_wait);
-
     return 0;
 }
