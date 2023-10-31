8b6a8056c3893bf950e52a82c31a0977edd7333c
Henonicks <sawsawsa777@gmail.com>
2023-10-31 18:32:58 +0200

    First commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# D++ CMake Template (templatebot)
+# C++ Discord bot made with [D++](https://dpp.dev), Bump Watcher 
 
-CMake template for a simple [D++](https://dpp.dev) bot. This template assumes that D++ is already installed.
+This bot watches when someone bumps a guild (Disboard bump) and gives them a dedicated role. Then, removes it in 2 hours.
 
 ## Compilation
 
@@ -13,12 +13,15 @@ If DPP is installed in a different location you can specify the root directory t
 
     cmake .. -DDPP_ROOT_DIR=<your-path>
 
-## Running the template bot
+## Running the bot
 
 Create a config.json in the directory above the build directory:
 
 ```json
-{ "token": "your bot token here" }
+{
+    "BOT_TOKEN" : "your bot token here",
+    "BUMPER_ROLE_ID" : "ID of the role to be given" 
+}
 ```
 
 Start the bot:
@@ -32,4 +35,4 @@ You can add as many header files and .cpp files into the src and include folders
 
 ## Renaming the bot
 
-To rename the bot, search and replace "templatebot" in the `CMakeLists.txt` with your new bots name and then rename the templatebot folder in include. Rerun `cmake ..` from the `build` directory and rebuild.
+To rename the bot, search and replace "bumpwatcher" in the `CMakeLists.txt` with your new bots name and then rename the bumpwatcher folder in include. Rerun `cmake ..` from the `build` directory and rebuild. You might need to re-create the `build` directory.
--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,56 @@
 #include <templatebot/templatebot.h>
-#include <sstream>
-
-/* When you invite the bot, be sure to invite it with the
- * scopes 'bot' and 'applications.commands', e.g.
- * https://discord.com/oauth2/authorize?client_id=940762342495518720&scope=bot+applications.commands&permissions=139586816064
- */
 
 using json = nlohmann::json;
+constexpr uint64_t DISBOARD_ID = 302050872383242240;
 
-int main(int argc, char const *argv[])
-{
+int main() {
     json configdocument;
     std::ifstream configfile("../config.json");
     configfile >> configdocument;
 
-    /* Setup the bot */
-    dpp::cluster bot(configdocument["token"]);
+    dpp::cluster bot(configdocument["BOT_TOKEN"], dpp::i_message_content | dpp::i_default_intents);
+    const dpp::snowflake& role_id = configdocument["BUMPER_ROLE_ID"];
 
-    /* Output simple log messages to stdout */
     bot.on_log(dpp::utility::cout_logger());
 
-    /* Handle slash command */
-    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
-         if (event.command.get_command_name() == "ping") {
-            event.reply("Pong!");
+    bot.on_slashcommand([](const dpp::slashcommand_t& event) -> void {
+        if (event.command.get_command_name() == "bump") {
+            dpp::embed embed = dpp::embed()
+                    .set_color(dpp::colors::sti_blue)
+                    .set_title("Title")
+                    .set_image("https://disboard.org/images/bot-command-image-bump.png");
+            event.reply(dpp::message().add_embed(embed));
         }
     });
 
-    /* Register slash command here in on_ready */
-    bot.on_ready([&bot](const dpp::ready_t& event) {
-        /* Wrap command registration in run_once to make sure it doesnt run on every full reconnection */
-        if (dpp::run_once<struct register_bot_commands>()) {
-            bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
+    bot.on_message_create([&bot, role_id](const dpp::message_create_t& event) -> void {
+        const dpp::message& msg = event.msg;
+        if (msg.author.id == DISBOARD_ID && msg.embeds.size() == 1) {
+            if (msg.embeds[0].image->url == "https://disboard.org/images/bot-command-image-bump.png") {
+                dpp::user user = msg.interaction.usr;
+                bot.guild_member_add_role(msg.guild_id, user.id, role_id, [&bot](const dpp::confirmation_callback_t& callback) -> void {
+                    if (callback.is_error()) {
+                        bot.log(dpp::ll_error, callback.get_error().message);
+                    }
+                });
+                bot.start_timer([&bot, msg, role_id, user](const dpp::timer& h) -> void {
+                    bot.guild_member_remove_role(msg.guild_id, user.id, role_id, [&bot](const dpp::confirmation_callback_t& callback) -> void {
+                        if (callback.is_error()) {
+                            bot.log(dpp::ll_error, callback.get_error().message);
+                        }
+                    });
+                    bot.stop_timer(h);
+                }, 7200);
+            }
         }
     });
 
-    /* Start the bot */
+    bot.on_ready([&bot](const dpp::ready_t&) -> void {
+        if (dpp::run_once <struct register_bot_commands>()) {
+            bot.global_bulk_command_create({ dpp::slashcommand("bump", "bump", bot.me.id) });
+        }
+    });
+
     bot.start(dpp::st_wait);
 
     return 0;
