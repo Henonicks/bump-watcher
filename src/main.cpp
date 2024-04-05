29c48d44e4353bc8390714fbfbc318f484b7dad9
Jaskowicz1 <archiejaskowicz@gmail.com>
2024-04-05 15:24:49 +0100

    fix: initialisation changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <templatebot/templatebot.h>
 
 using json = nlohmann::json;
-constexpr uint64_t DISBOARD_ID = 302050872383242240;
+constexpr uint64_t DISBOARD_ID{302050872383242240};
 
 // This will only return a value on Linux.
-int64_t proc_self_value(const std::string& find_token) {
-	int64_t ret = 0;
+int64_t proc_self_value(const std::string_view find_token) {
+	int64_t ret{0};
 	std::ifstream self_status("/proc/self/status");
 	while (self_status) {
 		std::string token;
@@ -25,36 +25,36 @@ int64_t rss() {
 
 int main() {
 	json config_document;
-	std::ifstream config_file("../config.json");
+	std::ifstream config_file{"../config.json"};
 	config_file >> config_document;
 
 	/* Create bot */
-	dpp::cluster bot(config_document["BOT_TOKEN"], dpp::i_message_content | dpp::i_default_intents, 0, 0, 1, true, dpp::cache_policy::cpol_none);
+	dpp::cluster bot{config_document["BOT_TOKEN"], dpp::i_message_content | dpp::i_default_intents, 0, 0, 1, true, dpp::cache_policy::cpol_none};
 
 	bot.on_log(dpp::utility::cout_logger());
 
 	bot.on_slashcommand([&bot](const dpp::slashcommand_t& event) {
 		if (event.command.get_command_name() == "set_role") {
-			auto role = std::get<dpp::snowflake>(event.get_parameter("role"));
+			auto role{std::get<dpp::snowflake>(event.get_parameter("role"))};
 
 			if (role.empty()) {
-				event.reply(dpp::message("You didn't specify a role!").set_flags(dpp::m_ephemeral));
+				event.reply(dpp::message{"You didn't specify a role!"}.set_flags(dpp::m_ephemeral));
 				return;
 			}
 
 			json j;
 			j["ROLE_ID"] = role.str();
-			std::ofstream guild_config("../" + event.command.guild_id.str() + ".json");
+			std::ofstream guild_config{"../" + event.command.guild_id.str() + ".json"};
 			guild_config << j.dump() << "\n";
 			guild_config.close();
 
-			event.reply(dpp::message("Updated the role!").set_flags(dpp::m_ephemeral));
+			event.reply(dpp::message{"Updated the role!"}.set_flags(dpp::m_ephemeral));
 		} else if (event.command.get_command_name() == "info") {
-			std::ifstream guild_config("../" + event.command.guild_id.str() + ".json");
+			std::ifstream guild_config{"../" + event.command.guild_id.str() + ".json"};
 			dpp::snowflake role_id{};
 
 			if (guild_config.good()) {
-				json j;
+				json j{};
 				guild_config >> j;
 				role_id = j["ROLE_ID"];
 			}
@@ -91,7 +91,7 @@ int main() {
 			return;
 		}
 
-		std::ifstream guild_config("../" + event.msg.guild_id.str() + ".json");
+		std::ifstream guild_config{"../" + event.msg.guild_id.str() + ".json"};
 
 		if (guild_config.bad()) {
 			event.reply("Could not give a role based off this message as the role has not been setup! Please use `/set_role`.");
@@ -127,26 +127,26 @@ int main() {
 
 	bot.on_ready([&bot](const dpp::ready_t& event) {
 		if (dpp::run_once<struct setup_commands>()) {
-			dpp::slashcommand setrole_command("set_role", "Set the role that Bump Watcher should add to the the most recent bumper!", bot.me.id);
+			dpp::slashcommand setrole_command{"set_role", "Set the role that Bump Watcher should add to the the most recent bumper!", bot.me.id};
 			setrole_command.add_option(
-				dpp::command_option(dpp::co_role, "role", "The role that the bot should add for the most recent bumper!", true)
+				dpp::command_option{dpp::co_role, "role", "The role that the bot should add for the most recent bumper!", true}
 			);
 
-			dpp::slashcommand info_command("info", "Get information about the bot!", bot.me.id);
+			dpp::slashcommand info_command{"info", "Get information about the bot!", bot.me.id};
 
 			bot.global_bulk_command_create({ setrole_command, info_command });
 		}
 
 		if (dpp::run_once<struct presence_timer>()) {
 			bot.current_application_get([&bot](const dpp::confirmation_callback_t& callback) {
-				auto app = callback.get<dpp::application>();
-				bot.set_presence(dpp::presence(dpp::ps_online, dpp::at_watching, "DISBOARD on " + std::to_string(app.approximate_guild_count) + " servers!"));
+				auto app{callback.get<dpp::application>()};
+				bot.set_presence(dpp::presence{dpp::ps_online, dpp::at_watching, "DISBOARD on " + std::to_string(app.approximate_guild_count) + " servers!"});
 			});
 
 			bot.start_timer([&bot](const dpp::timer& timer) {
 				bot.current_application_get([&bot](const dpp::confirmation_callback_t& callback) {
-					auto app = callback.get<dpp::application>();
-					bot.set_presence(dpp::presence(dpp::ps_online, dpp::at_watching, "DISBOARD on " + std::to_string(app.approximate_guild_count) + " servers!"));
+					auto app{callback.get<dpp::application>()};
+					bot.set_presence(dpp::presence{dpp::ps_online, dpp::at_watching, "DISBOARD on " + std::to_string(app.approximate_guild_count) + " servers!"});
 				});
 			}, 120);
 		}
